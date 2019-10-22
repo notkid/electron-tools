532c300d86450f89d3ee94471212e8fd0a05ad34
notkid <liuhaiyang1992@hotmail.com>
2019-10-22 17:48:35 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import Button from './components/Menu'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{ "-webkit-app-region": "drag" }}>
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p
+          className="App-link"
+        >
+          工具
+        </p>
+      </header>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
